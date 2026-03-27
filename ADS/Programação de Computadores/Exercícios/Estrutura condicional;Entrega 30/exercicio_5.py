6381e5f4a381a188d6cfb61a43b9e8f2d3dda014
Nathan-GN-Santos <nathan.g.n.santos@gmail.com>
2026-03-27 10:29:22 -0300

    sincroniza logo

--- ADS/Programação de Computadores/Exercícios/Estrutura condicional;Entrega 30/exercicio_5.py
+++ ADS/Programação de Computadores/Exercícios/Estrutura condicional;Entrega 30/exercicio_5.py
@@ -1,3 +1,12 @@
 peso_do_peixe = float(input("Digite o peso do peixe: "))
 
 if peso_do_peixe > 50:
+    E = peso_do_peixe - 50
+    M = E * 4
+
+else :
+    E = 0
+    M = 0
+
+print(f"Excesso: {E:.2f} kg")
+print(f"Multa: R${M:.2f}")
