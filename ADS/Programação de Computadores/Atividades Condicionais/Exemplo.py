630582d9a6e7797492c62b8d55d26db6e9f48764
Nathan-GN-Santos <nathan.g.n.santos@gmail.com>
2026-03-27 09:59:25 -0300

    vai pra casa

---
+++ ADS/Programação de Computadores/Atividades Condicionais/Exemplo.py
@@ -0,0 +1,17 @@
+numero = int(input("Digite um número:")) 
+
+if numero == 0:
+    print("nulo") 
+
+if numero > 0:
+    print("Positivo")
+
+if numero < 0:
+    print("Negativo")
+
+    
+#else 
+ #   print("banana açucarada")
+
+#se condição F então --> exib
+
