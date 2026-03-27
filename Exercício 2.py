4ce56e911f8f3052b22de62fc07a1dbdd08f38b4
Nathan-GN-Santos <nathan.g.n.santos@gmail.com>
2026-03-27 08:05:56 -0300

    Commit 2

---
+++ Exercício 2.py
@@ -0,0 +1,37 @@
+# Fazer um menu de opções
+# A- Cadastrar 
+# B- Alterar
+# C- Deletar
+# D- Sair
+
+# Depois fazer um print dizendo oq aconteceu de acordo com a letra escolhida.
+#
+
+#opcao = input("Escolha sua opção entre: A, B, C ou D:")
+
+#A = ( "Você escolheu a opção para se Cadastrar. Continuar?")
+#B = ( "Você escolheu a opção para se Alterar. Continuar?")
+#C = ( "Você escolheu a opção para se Deletar. Continuar?")
+#D = ( "Você escolheu a opção para se Sair. Continuar?")
+
+
+
+#if opcao == A:
+ #   print(f"A") 
+
+print("**Menu de opções**")
+print("A- Cadastrar")
+print("B- Alterar")
+print("C- Deletar")
+print("D- Sair")
+
+op = input("Escolha sua opção:").upper()
+
+
+if op == "A" :
+    print("Você escolheu a opção para se Cadastrar. Continuar?")
+elif op == "B":
+    print("Você escolheu a opção de Alterar. Continuar?")
+
+
+
