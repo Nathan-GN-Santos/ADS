355a0e5975017e759eb93034320f5782cc8fff9a
Nathan-GN-Santos <nathan.g.n.santos@gmail.com>
2026-03-27 12:28:11 -0300

    27.03.2026 a

--- Programação de Computadores/Exercícios/Estrutura condicional;Entrega 30/exercicio_9.py
+++ Programação de Computadores/Exercícios/Estrutura condicional;Entrega 30/exercicio_9.py
@@ -3,21 +3,28 @@ CPF = input("Digite seu CPF: ")
 renda_anual = float(input("Digite sua renda anual: "))
 numb_dependentes = int(input("Digite o número de dependentes: "))
 
+desconto = 189.59 * numb_dependentes
 renda_mensal = renda_anual / 12
-renda_liquida = renda_mensal - (189.59 * numb_dependentes)
+renda_liquida = renda_mensal - desconto
 
 if renda_liquida <= 2428.80 :
+    aliquota = 0
     print("Isento")
 elif renda_liquida <= 2826.65:
+    aliquota = 0.075
     print("Alíquota de 7,5%")
 elif renda_liquida <= 3751.05:
+    aliquota = 0.15
     print("Alíquota de 15%")
 elif renda_liquida <= 4664.68:
+    aliquota = 0.225
     print("Alíquota de 22,5%")
 else:
     print("Alíquota de 27,5%")
 
 
-imposto = 
+imposto =  renda_liquida * aliquota
 
-print(f"desconto: R${desconto:.2f}")
+
+print(f"Renda mensal: R${renda_mensal:.2f}")
+print(f"imposto: R${imposto:.2f}")
